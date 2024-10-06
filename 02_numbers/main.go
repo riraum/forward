e93ce135cee8e6cca44a3f6c1a7bef07ce669b19
nobe4 <nobe4@users.noreply.github.com>
2024-10-06 21:17:12 +0200

    fix(02_numbers): improve exercise slightly

--- 02_numbers/main.go
+++ 02_numbers/main.go
@@ -27,7 +27,7 @@ E.g.
     y = y * 2 // y is now 20
     ...
 
-Note: `x` and `y` must be of the same type.
+Note: In this example, `x` and `y` must be of the same type.
     See a TODO later in this file.
 
 */
@@ -37,7 +37,8 @@ func main() {
 	// Fix the following code:
 	var a int = 1
 	var b string = "3" // wrong type!
-	var c int = a + b  // we want c to be 4
+	var c int = a + b
+	fmt.Println(c) // want to print 4
 
 	// TODO: See how the code doesn't run because the types are mismatched.
 	// Fix the following code:
@@ -46,8 +47,11 @@ func main() {
 	fmt.Println(d + e) // we want to print 3.5
 
 	// TODO: Define a variable `x` of type `int` and set it to 10
+
 	// TODO: Define a variable `y` of type `int` and set it to 20
+
 	// TODO: Define a variable `z` without value, what should be its type?
+
 	// TODO: Print the value of `z` to the console.
 
 	// TODO: Define 2 variables of type `int` and compute their sum, difference,
@@ -56,4 +60,9 @@ func main() {
 
 	// TODO: Define a variable of type `int` and increment it by 1.
 	// Then decrement it by 1.
+	// Print the value of the variable after each operation.
+
+	// TODO: Define a variable with a value of 1.
+	// Increase its value by 0.5, 3 times.
+	// Print the value of the variable after each operation.
 }
