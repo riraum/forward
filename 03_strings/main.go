38357b690f015e7c6d8ccd51374fdbf6385d4bec
M <114847810+riraum@users.noreply.github.com>
2024-10-07 13:03:18 +0900

    Revert "Solve exercise 02"
    This reverts commit 4968a6d974de92b199c2cef43d47e2df7401a69f.

--- 03_strings/main.go
+++ 03_strings/main.go
@@ -42,45 +42,25 @@ func main() {
 
 	// TODO: Define a string variable called `greetings` with the value
 	// "Hello, World!"
-	var greetings string = "Hello, World!"
 
 	// TODO: Print the variable `greetings`.
-	fmt.Println(greetings)
 
 	// TODO: Print the length of the string in the variable `greetings`.
 	// Hint: it should be 13.
-	fmt.Println(len(greetings))
 
 	// TODO: Define a new variable called `name` containing your name.
 	// Add your name to the `greetings` variable and store the result in a new
 	// variable called `message`.
 	// Print the `message` variable.
-	var name string = "riraum"
-	var message string = greetings + name
-	fmt.Println(message)
 
 	// TODO: Define a new variable called `quotedName` containing your name in
 	// double quotes.
 	// Add your name to the `greetings` variable and store the result in a new
 	// variable called `quotedMessage`.
 	// Print the `quotedMessage` variable.
-	var quotedName string = `"riraum"`
-	var quotedMessage string = greetings + quotedName
-	fmt.Println(quotedMessage)
 
 	// TODO: Define a new variable called `repeatedLetter` containing the letter
 	// "a".
 	// Add the letter "a" to the variable `repeatedLetter` 5 times.
 	// Print the `repeatedLetter` variable after each addition.
-	var repeatedLetter string = "a"
-	repeatedLetter += repeatedLetter
-	fmt.Println(repeatedLetter)
-	repeatedLetter += repeatedLetter
-	fmt.Println(repeatedLetter)
-	repeatedLetter += repeatedLetter
-	fmt.Println(repeatedLetter)
-	repeatedLetter += repeatedLetter
-	fmt.Println(repeatedLetter)
-	repeatedLetter += repeatedLetter
-	fmt.Println(repeatedLetter)
 }
