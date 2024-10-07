207746d2f52d1c8771c0e51baf374eae217c886c
M <114847810+riraum@users.noreply.github.com>
2024-10-07 12:48:44 +0900

    Solve exercise 02

--- 02_numbers/main.go
+++ 02_numbers/main.go
@@ -36,33 +36,57 @@ func main() {
 	// TODO: See how the code doesn't run because the types are mismatched.
 	// Fix the following code:
 	var a int = 1
-	var b string = "3" // wrong type!
+	var b int = 3 // wrong type!
 	var c int = a + b
 	fmt.Println(c) // want to print 4
 
 	// TODO: See how the code doesn't run because the types are mismatched.
 	// Fix the following code:
-	var d int = 1
-	var e int = 2.5
+	var d float64 = 1
+	var e float64 = 2.5
 	fmt.Println(d + e) // we want to print 3.5
 
 	// TODO: Define a variable `x` of type `int` and set it to 10
+	var x int = 10
 
 	// TODO: Define a variable `y` of type `int` and set it to 20
+	var y int = 20
 
 	// TODO: Define a variable `z` with the sum of `x` and `y`, what should be
 	// its type?
 	// Print the value of `z`.
+	var z int = x + y
+	fmt.Println(z)
 
 	// TODO: Define 2 variables of type `int` and compute their sum, difference,
 	// product, quotient, and remainder. Print all the results.
 	// Hint: you can store the results in variables or print them directly.
+	var f int = 13
+	var g int = 37
+	var h int = f + g
+	var i int = f - g
+	var j int = f * g
+	var k int = f / g
+	var l int = f % g
+	fmt.Println(f, g, h, i, j, k, l)
 
 	// TODO: Define a variable of type `int` and increment it by 1.
 	// Then decrement it by 1.
 	// Print the value of the variable after each operation.
+	var m int = 99
+	m = m + 1
+	fmt.Println(m)
+	m = m - 1
+	fmt.Println(m)
 
 	// TODO: Define a variable with a value of 1.
 	// Increase its value by 0.5, 3 times.
 	// Print the value of the variable after each operation.
+	var n float64 = 1
+	n = n + 0.5
+	fmt.Println(n)
+	n = n + 0.5
+	fmt.Println(n)
+	n = n + 0.5
+	fmt.Println(n)
 }
