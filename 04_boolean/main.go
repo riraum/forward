b46560d6aabe589063ee57bb7fbbc6ee35e4001f
M <114847810+riraum@users.noreply.github.com>
2024-10-07 14:02:21 +0900

    Solve exercise 04_boolean

--- 04_boolean/main.go
+++ 04_boolean/main.go
@@ -74,15 +74,24 @@ func main() {
 	fmt.Println(true, false)
 
 	// TODO: Define a variable named `awesome` and set it to `true`.
+	var awesome bool = true
 
 	// TODO: Change the value of `awesome` to `false`.
 	// Print the value of `awesome`.
+	awesome = false
+	fmt.Println(awesome)
 
 	// TODO: Define two variables named `a` and `b` and set them to `true` and
 	// `false` respectively.
 	// Print the result of `a && b` and `a || b`, and `!a`.
 	// Change the value of `a` and `b`, and run the program again.
 	// Observe the results.
+	var a bool = true
+	var b bool = false
+	fmt.Println(a && b, a || b, !a)
+	a = false
+	b = true
+	fmt.Println(a && b, a || b, !a)
 
 	// TODO: Define a variable named `x` and set it to `1`.
 	// Define a variable named `y` and set it to `2`.
@@ -91,6 +100,19 @@ func main() {
 	// If it is not, print "x is not equal to y".
 	// Change the value of `y` to `1` and run the program again.
 	// Observe the results.
+	var x int = 1
+	var y int = 2
+	if x == y {
+		fmt.Println("x is equal to y")
+	} else {
+		fmt.Println("x is not equal to y")
+	}
+	y = 1
+	if x == y {
+		fmt.Println("x is equal to y")
+	} else {
+		fmt.Println("x is not equal to y")
+	}
 
 	// TODO: Define a variable named `firstName` and set it to "John".
 	// Define a variable named `lastName` and set it to "Doe".
@@ -104,9 +126,34 @@ func main() {
 	// If it is not, print "You are someone else".
 	// Change the value of `firstName` and `lastName` to different values and
 	// run the program again to show all cases.
+	var firstName string = "John"
+	var lastName string = "Doe"
+	if firstName == "John" && lastName == "Doe" {
+		fmt.Println("You are John Doe")
+	} else if firstName == "John" && lastName != "Doe" {
+		fmt.Println("You are another John")
+	} else if firstName != "John" && lastName == "Doe" {
+		fmt.Println("You are another Doe")
+	} else {
+		fmt.Println("You are someone else")
+	}
 
 	// Hint: there are 2 ways to do the previous exercise:
 	// 1. Use a single level of if-else statements
 	// 2. Use nested if-else statements
 	// TODO: Try both ways.
+	if firstName == "John" {
+		if lastName == "Doe" {
+			fmt.Println("You are John Doe")
+		} else {
+			fmt.Println("You are another John")
+		}
+	}
+	if firstName != "John" {
+		if lastName == "Doe" {
+			fmt.Println("You are another Doe")
+		} else {
+			fmt.Println("You are someone else")
+	}
+	}
 }
