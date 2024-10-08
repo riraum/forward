dbfe58a68af38e2de27b76a0a84398e9bd720ec4
M <114847810+riraum@users.noreply.github.com>
2024-10-08 12:05:24 +0900

    Uncomment tasks

--- 05_loops/main.go
+++ 05_loops/main.go
@@ -42,47 +42,47 @@ it's also the most explicit.
 */
 
 func main() {
-	// var i int = 0
-	// for {
-	// 	if i >= 10 {
-	// 		break
-	// 	}
-	// 	fmt.Println(i)
-	// 	i += 1
-	// }
+	var i int = 0
+	for {
+		if i >= 10 {
+			break
+		}
+		fmt.Println(i)
+		i += 1
+	}
 
 	// TODO: Write a loop that prints the numbers from 0 to 100, inclusive.
-	// var j int = 0
-	// for {
-	// 	if j > 100 {
-	// 		break
-	// 	}
-	// 	fmt.Println(j)
-	// 	j += 1
-	// }
+	var j int = 0
+	for {
+		if j > 100 {
+			break
+		}
+		fmt.Println(j)
+		j += 1
+	}
 
 	// TODO: Write a loop that prints the numbers from 100 to 0, inclusive.
-	// var k int = 100
-	// for {
-	// 	if k < 0 {
-	// 		break
-	// 	}
-	// 	fmt.Println(k)
-	// 	k -= 1
-	// }
+	var k int = 100
+	for {
+		if k < 0 {
+			break
+		}
+		fmt.Println(k)
+		k -= 1
+	}
 
 	// TODO: Write a loop that print the numbers from 0 to 100, inclusive, but
 	// only the even numbers.
-	// var l int = 0
-	// for {
-	// 	if l > 100{
-	// 		break
-	// 	}
-	// 	if l % 2 == 0 {
-	// 		fmt.Println(l)
-	// 		}
-	// 	l += 1
-	// }
+	var l int = 0
+	for {
+		if l > 100{
+			break
+		}
+		if l % 2 == 0 {
+			fmt.Println(l)
+			}
+		l += 1
+	}
 
 	// TODO: Write a loop that prints the first 100 odd numbers.
 	// Initialize counter
@@ -105,16 +105,16 @@ func main() {
 
 	// TODO: Define a variable and write a loop that calculates the sum of the
 	// numbers from 0 to 100, inclusive. Print the result.
-	// var n int = 0
-	// var sum int
-	// for {
-	// 	if n > 100 {
-	// 		break
-	// 	}
-	// 	sum += n
-	// 	n += 1
-	// }
-	// fmt.Println(sum)
+	var n int = 0
+	var sum int
+	for {
+		if n > 100 {
+			break
+		}
+		sum += n
+		n += 1
+	}
+	fmt.Println(sum)
 
 	// TODO: Write a fizzbuzz.
 	// Fizz buzz is a group word game for children to teach them about division.
@@ -123,24 +123,24 @@ func main() {
 	//  with the word "buzz", and any number divisible
 	// by both three and five with the word "fizzbuzz".
 	// If none applies, return value.
-	// var o int = 1
-	// var resultOne string = "fizz"
-	// var resultTwo string = "buzz"
-	// for {
-	// 	if o > 15 {
-	// 		break
-	// 	}
-	// 	if o % 3 == 0 && o % 5 == 0{
-	// 		fmt.Println(resultOne + resultTwo)
-	// 	} else if o % 3 == 0 {
-	// 		fmt.Println(resultOne)
-	// 	} else if o % 5 == 0 {
-	// 		fmt.Println(resultTwo)
-	// 	} else {
-	// 		fmt.Println(o)
-	// 	}
-	// 	o += 1
-	// }
+	var o int = 1
+	var resultOne string = "fizz"
+	var resultTwo string = "buzz"
+	for {
+		if o > 15 {
+			break
+		}
+		if o % 3 == 0 && o % 5 == 0{
+			fmt.Println(resultOne + resultTwo)
+		} else if o % 3 == 0 {
+			fmt.Println(resultOne)
+		} else if o % 5 == 0 {
+			fmt.Println(resultTwo)
+		} else {
+			fmt.Println(o)
+		}
+		o += 1
+	}
 	// Nested logic, to revisit later
 	// for {
 	// 	if o > 15 {
@@ -162,17 +162,17 @@ func main() {
 
 	// TODO: Write a loop that prints the first 20 Fibonacci numbers.
 	// Fibonacci sequence is a sequence in which each number is the sum of the two preceding ones
-	// var r int = 0
-	// var s int = 1
-	// var t int = 0
-	// for {
-	// 	if t > 20 {
-	// 		break
-	// 	}
-	// 	fmt.Println(r)
-	// 	fmt.Println(s)
-	// 	r = r + s
-	// 	s = r + s
-	// 	t += 1
-	// }
+	var r int = 0
+	var s int = 1
+	var t int = 0
+	for {
+		if t > 20 {
+			break
+		}
+		fmt.Println(r)
+		fmt.Println(s)
+		r = r + s
+		s = r + s
+		t += 1
+	}
 }
