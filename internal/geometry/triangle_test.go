c5b1a21435f1ec979e55a72edbe69a9fb6d5802c
M <114847810+riraum@users.noreply.github.com>
2024-11-11 18:02:14 +0900

    WIP triangle_test

--- internal/geometry/triangle_test.go
+++ internal/geometry/triangle_test.go
@@ -2,7 +2,16 @@ package geometry
 
 import "testing"
 
-func TestArea(t *testing.T) {
+func TestAddXY(t *testing.T) {
 	tests := []struct {
+		ty   Triangle
+		want float64
+	}{
+	Triangle{
+		A: Point{X: 0, Y: 0},
+		B: Point{X: 0, Y: 4},
+		C: Point{X: 3, Y: 0},
+		0, 4, 3,}
 	}
-}
+	}
+
