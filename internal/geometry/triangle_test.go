b113322603c243c113ace869c6c1bbfcf99eb2eb
M <114847810+riraum@users.noreply.github.com>
2024-11-11 19:01:52 +0900

    More triangle test attempts

--- internal/geometry/triangle_test.go
+++ internal/geometry/triangle_test.go
@@ -4,14 +4,19 @@ import "testing"
 
 func TestAddXY(t *testing.T) {
 	tests := []struct {
-		ty   Triangle
-		want float64
+		ta   Triangle
+		want float64, float64, float64
 	}{
-	Triangle{
-		A: Point{X: 0, Y: 0},
-		B: Point{X: 0, Y: 4},
-		C: Point{X: 3, Y: 0},
-		0, 4, 3,}
+		Triangle{
+			Point{0, 0},
+			Point{0, 4},
+			Point{3, 0},
+			0, 4, 3},
 	}
+	for _, test := range tests {
+		got := test.ta.AddXY()
+		if got != test.want {
+			t.Errorf("AddxXY(%v) = %v, want%v", test.ta, got, test.want)
+		}
 	}
-
+}
