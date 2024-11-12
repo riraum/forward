ba1c53ef21177b718b9fd65d8d76f4caba0a3273
M <114847810+riraum@users.noreply.github.com>
2024-11-12 14:02:06 +0900

    WIP triangle test

--- internal/geometry/triangle_test.go
+++ internal/geometry/triangle_test.go
@@ -5,14 +5,16 @@ import "testing"
 func TestAddXY(t *testing.T) {
 	tests := []struct {
 		ta   Triangle
-		want []float64
+		want1 float64
+		want2 float64
+		want3 float64
 	}{
-		Triangle{
+		Triangle{ 
 			Point{0, 0},
 			Point{0, 4},
 			Point{3, 0},
 			{0, 4, 3, float64},
-	},
+	}
 	for _, test := range tests {
 		got := test.ta.AddXY()
 		if got != test.want {
