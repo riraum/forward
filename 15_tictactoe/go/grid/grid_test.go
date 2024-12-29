9e455ebf072472775709d68017f6dea894a4833c
M <114847810+riraum@users.noreply.github.com>
2024-12-29 20:40:57 +0700

    Add IsWin test

--- 15_tictactoe/go/grid/grid_test.go
+++ 15_tictactoe/go/grid/grid_test.go
@@ -20,3 +20,41 @@ func TestString(t *testing.T) {
 		t.Errorf("testGrid.String() = %v; want %v", test, wantedResult)
 	}
 }
+
+func TestIsWin(t *testing.T) {
+	tests := []struct {
+		g     Grid
+		want1 bool
+		want2 string
+	}{
+		{
+			g: Grid{
+				Cells: [9]int{1, 1, 1, 0, 2, 1, 0, 1, 1},
+			},
+			want1: true,
+			want2: "X",
+		},
+		{
+			g: Grid{
+				Cells: [9]int{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			},
+			want1: false,
+			want2: ""},
+	}
+
+	for _, test := range tests {
+		got1, got2 := test.g.IsWin()
+		if got1 != test.want1 && got2 != test.want2 {
+			t.Errorf("IsWin(%v) = %v, %v, want %v and %v", test.g, got1, got2, test.want1, test.want2)
+		}
+	}
+}
+
+// testGrid := Grid{
+// 	Cells: [9]int{1, 1, 1, 0, 2, 1, 0, 1, 1},
+// }
+// testBool, testValue := testGrid.IsWin()
+// wantedResult := true, "X"
+// if testBool != wantedResult && testValue != wantedResult {
+
+// }
