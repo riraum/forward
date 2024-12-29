2478c28a75332d6dc3b656f2925edf69ae51fe9f
M <114847810+riraum@users.noreply.github.com>
2024-12-29 19:36:14 +0700

    Add string grid function test

--- 15_tictactoe/go/grid/grid_test.go
+++ 15_tictactoe/go/grid/grid_test.go
@@ -3,4 +3,48 @@ package grid
 import "testing"
 
 func TestString(t *testing.T) {
+	testGrid := Grid{
+		Cells: [9]int{1, 1, 1, 0, 2, 1, 0, 1, 1},
+	}
+	test := testGrid.String()
+	wantedResult := `
++---+---+---+
+| 1 | 1 | 1 |
++---+---+---+
+| 0 | 2 | 1 |
++---+---+---+
+| 0 | 1 | 1 |
++---+---+---+`
+	if test != wantedResult {
+
+		t.Errorf("testGrid.String() = %v; want %v", test, wantedResult)
+	}
 }
+
+// 	testGrid := []struct {
+// 		g    Grid
+// 		want string
+// 	}{
+// 		{
+// 			g: Grid{
+// 				Cells: [9]int{1, 1, 1, 0, 2, 1, 0, 1, 1},
+// 			},
+// 			want: `+---+---+---+
+// | 1 | 1 | 1 |
+// +---+---+---+
+// | 0 | 2 | 1 |
+// +---+---+---+
+// | 0 | 1 | 1 |
+// +---+---+---+}`,
+// 		},
+// 	}
+// 	test := testGrid.String()
+// }
+
+// +---+---+---+
+// | 1 | 1 | 1 |
+// +---+---+---+
+// | 0 | 2 | 1 |
+// +---+---+---+
+// | 0 | 1 | 1 |
+// +---+---+---+
