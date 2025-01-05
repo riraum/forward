b43eaad6c38dd818db052faabce7e734f784a714
M <114847810+riraum@users.noreply.github.com>
2025-01-05 17:44:42 +0700

    Improve want namings

--- 15_tictactoe/go/grid/grid_test.go
+++ 15_tictactoe/go/grid/grid_test.go
@@ -27,28 +27,28 @@ func TestString(t *testing.T) {
 func TestIsWin(t *testing.T) {
 	tests := []struct {
 		g     Grid
-		want1 bool
-		want2 string
+		wantIsWin bool
+		wantWinner string
 	}{
 		{
 			g: Grid{
 				Cells: [9]int{1, 1, 1, 0, 2, 1, 0, 1, 1},
 			},
-			want1: true,
-			want2: "X",
+			wantIsWin: true,
+			wantWinner: "X",
 		},
 		{
 			g: Grid{
 				Cells: [9]int{0, 0, 0, 0, 0, 0, 0, 0, 0},
 			},
-			want1: false,
-			want2: ""},
+			wantIsWin: false,
+			wantWinner: ""},
 	}
 
 	for _, test := range tests {
 		got1, got2 := test.g.IsWin()
-		if got1 != test.want1 && got2 != test.want2 {
-			t.Errorf("IsWin(%v) = %v, %v, want %v and %v", test.g, got1, got2, test.want1, test.want2)
+		if got1 != test.wantIsWin && got2 != test.wantWinner {
+			t.Errorf("IsWin(%v) = %v, %v, want %v and %v", test.g, got1, got2, test.wantIsWin, test.wantWinner)
 		}
 	}
 }
