d9732255a4ef526767502da910817b2af63641c7
M <114847810+riraum@users.noreply.github.com>
2025-01-06 22:56:33 +0700

    Start game package logic assembly

--- 15_tictactoe/go/game/game.go
+++ 15_tictactoe/go/game/game.go
@@ -9,11 +9,16 @@ import (
 )
 
 /*
+Initialize empty grid
+Output empty grid to help understand the concept?
 Allow player to choose X or O?
 Ask player to select a grid index
+Add player marker to correct index of grid
 Check if there is a winner, if yes, exit
 Output grid to visualize choice?
+Get freecells
 Make random computer choice, based on freecells
+Add computer marker to correct index of grid
 Output grid to visualize result
 Output free cells
 Ask player to select a grid index
@@ -21,6 +26,25 @@ Check if there is a winner, if yes, exit, if no, continue from the start
 */
 
 func Play() {
+	// Initialize empty grid
+	g := grid.Grid{
+		Cells: [9]string{" ", " ", " ", " ", " ", " ", " ", " ", " "},
+	}
+	// Output empty grid to help understand the concept?
+	fmt.Println(g.String())
+	// Ask player to select a grid index
+	move := io.Read("What's your choice? (Enter index 0-8 if not taken before)")
+	fmt.Println("You chose", move)
+	// Add player marker to correct index of grid
+	g.Cells[0] = move
+	// Check if there is a winner
+	fmt.Println(g.IsWin())
+	// Get freecells
+	freeCellSlice := []int{}
+	freeCellSlice = append(freeCellSlice, g.FreeCells()...)
+	// Make random computer choice, based on freecells
+	random.Choose(freeCellSlice)
+	// Output grid to visualize result
 
 }
 
