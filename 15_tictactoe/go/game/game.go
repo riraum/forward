5632bf3eb9fb92c793aee9015fe8e3b774b1de25
M <114847810+riraum@users.noreply.github.com>
2025-01-07 20:40:27 +0700

    Fix break logic in case of win

--- 15_tictactoe/go/game/game.go
+++ 15_tictactoe/go/game/game.go
@@ -33,10 +33,10 @@ func Play() {
 	}
 
 	for i := 0; i <= len(g.Cells); i++ {
-		if i == len(g.Cells) {
-			fmt.Println("Grid full, no win!")
-			break
-		}
+		// if i == len(g.Cells) {
+		// 	fmt.Println("Grid full, no win!")
+		// 	break
+		// }
 		// Output empty grid to help understand the concept?
 		fmt.Println(g.String())
 		// Ask player to select a grid index
@@ -48,9 +48,14 @@ func Play() {
 		fmt.Println(g.String())
 		// Check if there is a winner
 		fmt.Println(g.IsWin())
-		if g.IsWin() == true, "X won" || g.IsWin() == true, "O won" {
+		// WIP break condition
+		win, _ := g.IsWin()
+		if win {
 			break
 		}
+		// if g.IsWin() == true, "X won" || g.IsWin() == true, "O won" {
+		// 	break
+		// }
 		// Get freecells
 		freeCellSlice := []int{}
 		freeCellSlice = append(freeCellSlice, g.FreeCells()...)
@@ -62,47 +67,52 @@ func Play() {
 		fmt.Println(g.String())
 		// Check if there is a winner, if yes, exit, if no, continue from the start
 		fmt.Println(g.IsWin())
+		// WIP break condition
+		win2, _ := g.IsWin()
+		if win2 {
+			break
+		}
 	}
 }
 
-func Debug() {
-	// TODO
-	// g := grid.Grid{}
-	// fmt.Println(g.String())
+// func Debug() {
+// 	// TODO
+// 	// g := grid.Grid{}
+// 	// fmt.Println(g.String())
 
-	g := grid.Grid{
-		Cells: [9]string{}}
+// 	g := grid.Grid{
+// 		Cells: [9]string{}}
 
-	// gameGrid := Grid{
-	// Cells: [9]int{}}
-	// X := "X"
-	// O := "O"
-	// Hardcoded values for debugging
-	g.Cells[0] = "X"
-	g.Cells[1] = "X"
-	g.Cells[2] = "X"
-	g.Cells[3] = " "
-	g.Cells[4] = "O"
-	g.Cells[5] = "X"
-	g.Cells[6] = " "
-	g.Cells[7] = "X"
-	g.Cells[8] = "X"
+// 	// gameGrid := Grid{
+// 	// Cells: [9]int{}}
+// 	// X := "X"
+// 	// O := "O"
+// 	// Hardcoded values for debugging
+// 	g.Cells[0] = "X"
+// 	g.Cells[1] = "X"
+// 	g.Cells[2] = "X"
+// 	g.Cells[3] = " "
+// 	g.Cells[4] = "O"
+// 	g.Cells[5] = "X"
+// 	g.Cells[6] = " "
+// 	g.Cells[7] = "X"
+// 	g.Cells[8] = "X"
 
-	fmt.Println(g.String())
+// 	fmt.Println(g.String())
 
-	fmt.Println(g.IsWin())
+// 	fmt.Println(g.IsWin())
 
-	fmt.Println(g.FreeCells())
+// 	fmt.Println(g.FreeCells())
 
-	//debug int slice
-	intSlice := []int{1, 5, 6, 8}
+// 	//debug int slice
+// 	intSlice := []int{1, 5, 6, 8}
 
-	fmt.Println(random.Choose(intSlice))
+// 	fmt.Println(random.Choose(intSlice))
 
-	// promptText := "Hurr durr"
+// 	// promptText := "Hurr durr"
 
-	// fmt.Println(io.Read(promptText))
+// 	// fmt.Println(io.Read(promptText))
 
-	move := io.Read("What's your move?")
-	fmt.Println("You chose", move)
-}
+// 	move := io.Read("What's your move?")
+// 	fmt.Println("You chose", move)
+// }
