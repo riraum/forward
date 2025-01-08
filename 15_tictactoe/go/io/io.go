792c3d9cf92094524b12e05993a938a8cd59ae31
M <114847810+riraum@users.noreply.github.com>
2025-01-08 20:00:45 +0700

    Initialize

--- 15_tictactoe/go/io/io.go
+++ 15_tictactoe/go/io/io.go
@@ -2,6 +2,11 @@ package io
 
 import "fmt"
 
+/*
+Input validation:
+Create loop
+Break loop if input is an integer, else keep looping
+*/
 func Read(prompt string) int {
 	var input int
 	fmt.Printf("%s > ", prompt)
