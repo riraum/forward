4a45edb87f8c8969be84a7da15e48ff99fdf76b2
M <114847810+riraum@users.noreply.github.com>
2025-01-08 20:35:33 +0700

    Basic attempt

--- 15_tictactoe/go/io/io.go
+++ 15_tictactoe/go/io/io.go
@@ -9,7 +9,18 @@ Break loop if input is an integer, else keep looping
 */
 func Read(prompt string) int {
 	var input int
-	fmt.Printf("%s > ", prompt)
-	fmt.Scan(&input)
+	for i := 0; i <= 3; i++ {
+
+		fmt.Printf("%s > ", prompt)
+		value, err := fmt.Scan(&input)
+		if err != nil {
+			continue
+		}
+		if value == 0 || value == 1 || value == 2 || value == 3 || value == 4 || value == 5 || value == 6 || value == 7 || value == 8 {
+			break
+		} else {
+			break
+		}
+	}
 	return input
 }
