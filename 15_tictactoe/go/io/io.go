363414b9819a1a1121493b71624b215dd0c7859b
M <114847810+riraum@users.noreply.github.com>
2025-01-08 23:00:57 +0700

    Improve logic

--- 15_tictactoe/go/io/io.go
+++ 15_tictactoe/go/io/io.go
@@ -6,6 +6,7 @@ import "fmt"
 Input validation:
 Create loop
 Break loop if input is an integer, else keep looping
+Break loop if input is on a free cell, else keep looping
 */
 func Read(prompt string) int {
 	var input int
@@ -15,10 +16,10 @@ func Read(prompt string) int {
 		value, err := fmt.Scan(&input)
 		if err != nil {
 			continue
-		}
-		if value == 0 || value == 1 || value == 2 || value == 3 || value == 4 || value == 5 || value == 6 || value == 7 || value == 8 {
-			break
 		} else {
+			fmt.Println(err)
+		}
+		if value >= 0 && value <= 8 {
 			break
 		}
 	}
