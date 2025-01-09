ac1fc853d3f20006ce4dd1ea271b95b5a5022694
M <114847810+riraum@users.noreply.github.com>
2025-01-09 19:57:07 +0700

    Start to split validation and input loop

--- 15_tictactoe/go/io/io.go
+++ 15_tictactoe/go/io/io.go
@@ -8,20 +8,50 @@ Create loop
 Break loop if input is an integer, else keep looping
 Break loop if input is on a free cell, else keep looping
 */
+
 func Read(prompt string) int {
 	var input int
 	for i := 0; i <= 3; i++ {
 
 		fmt.Printf("%s > ", prompt)
 		value, err := fmt.Scan(&input)
-		if err == nil {
-			fmt.Println(err)
-		} else {
+		if valid(value, err) {
 			continue
-		}
-		if value >= 0 && value <= 8 {
+		} else {
+			fmt.Println(err)
 			break
 		}
 	}
 	return input
 }
+
+func valid(value int, err error) bool {
+	// var result bool
+	if err != nil {
+		return true
+	} else {
+		fmt.Println(err)
+		if value >= 0 && value <= 8 {
+			return false
+		}
+	}
+	return false
+}
+
+// func Read(prompt string) int {
+// 	var input int
+// 	for i := 0; i <= 3; i++ {
+
+// 		fmt.Printf("%s > ", prompt)
+// 		value, err := fmt.Scan(&input)
+// 		if err != nil {
+// 			continue
+// 		} else {
+// 			fmt.Println(err)
+// 			if value >= 0 && value <= 8 {
+// 				break
+// 			}
+// 		}
+// 	}
+// 	return input
+// }
