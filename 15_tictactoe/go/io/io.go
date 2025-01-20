273f6442d96861bb501a1752fbf56f79d087ce9d
M <114847810+riraum@users.noreply.github.com>
2025-01-20 21:44:07 +0700

    WIP duplicate cell taken

--- 15_tictactoe/go/io/io.go
+++ 15_tictactoe/go/io/io.go
@@ -12,6 +12,8 @@ Break loop if input is an integer, else keep looping
 Break loop if input is on a free cell, else keep looping
 */
 
+var takenCells []int
+
 func Read(prompt string) int {
 	var input string
 	for {
@@ -20,6 +22,7 @@ func Read(prompt string) int {
 
 		if valid(input) {
 			validInput, _ := strconv.Atoi(input)
+			takenCells = append(takenCells, validInput)
 			return validInput
 		}
 	}
@@ -27,10 +30,18 @@ func Read(prompt string) int {
 
 func valid(input string) bool {
 	intInput, err := strconv.Atoi(input)
+	// var value int
 	if err != nil {
 		fmt.Print("Invalid input! Only single digit numbers allowed!\n")
 		return false
 	}
+
+	for value := range input {
+		if value == intInput {
+			fmt.Println("Already taken")
+		}
+	}
+
 	if intInput >= 0 && intInput <= 8 {
 		return true
 	}
@@ -39,3 +50,30 @@ func valid(input string) bool {
 	return false
 
 }
+
+/*
+Check user input and return invalid if cell index is already chosen
+
+Existing code, `grid.FreeCells`
+Used in game.go with `freeCellSlice` to print the free cells
+
+Find a way to loop? the user user input through the `freeCellSlice` and return the correct boolean
+
+Can't import and use freeCellSlice from `game.go` due to import cycle.
+
+Can import other package `grid.go`
+*/
+
+// func freeCell(input int) bool {
+// 		g := grid.Grid{
+// 		Cells: [9]string{" ", " ", " ", " ", " ", " ", " ", " ", " "},
+// 	}
+// 	freeCellSlice := g.FreeCells()
+// 	for index, value := range intInput {
+// 		if intInput == freeCellSlice {
+// 			fmt.Println("Freecell error")
+// 			break
+// 		}
+// 	}
+
+// }
