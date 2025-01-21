9cd810b7310120f21566dc599e20563f60b2dee0
M <114847810+riraum@users.noreply.github.com>
2025-01-21 17:50:02 +0700

    Re add emptyGrid

--- 15_tictactoe/go/game/game.go
+++ 15_tictactoe/go/game/game.go
@@ -29,9 +29,6 @@ Check if there is a winner, if yes, exit, if no, continue from the start
 func Play() {
 	// Initialize empty grid
 	g := grid.EmptyGrid()
-	// g := grid.Grid{
-	// 	Cells: [9]string{" ", " ", " ", " ", " ", " ", " ", " ", " "},
-	// }
 	fmt.Println(g.String())
 	for i := 0; i <= 9; i++ {
 		// Output empty grid to help understand the concept?
