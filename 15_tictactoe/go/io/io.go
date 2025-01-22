288d3e4f35d8ce096aceaaf1271395fd54685042
M <114847810+riraum@users.noreply.github.com>
2025-01-22 23:03:53 +0700

    Remove debug code, improve wording

--- 15_tictactoe/go/io/io.go
+++ 15_tictactoe/go/io/io.go
@@ -5,16 +5,6 @@ import (
 	"strconv"
 )
 
-/*
-Input validation:
-Create loop
-Break loop if input is an integer, else keep looping
-Break loop if input is on a free cell, else keep looping
-*/
-
-// Logic to check if duplicate user input only
-// var takenCells []int
-
 func Read(prompt string, freeCellSlice []int) int {
 	var input string
 	for {
@@ -23,36 +13,25 @@ func Read(prompt string, freeCellSlice []int) int {
 
 		if valid(input, freeCellSlice) {
 			validInput, _ := strconv.Atoi(input)
-			// to remove
-			// takenCells = append(takenCells, validInput)
 			return validInput
 		}
 	}
 }
 
 func valid(input string, freeCellSlice []int) bool {
-	// debug
-	fmt.Println("Debug freeSlice", freeCellSlice)
 	intInput, err := strconv.Atoi(input)
-	// var value int
+
 	if err != nil {
 		fmt.Print("Invalid input! Only single digit numbers allowed!\n")
 		return false
 	}
 
-	for index, value := range freeCellSlice {
-		// debug
-		fmt.Println("Debug index", index)
-		fmt.Println("Debug value", value)
-		fmt.Println("Debug intInput", intInput)
+	for _, value := range freeCellSlice {
 		if intInput == value {
-			// debug
-			fmt.Printf("%v not taken\n", value)
 			return true
 		}
 	}
 
-	fmt.Println("Print invalid input, already taken", intInput)
+	fmt.Printf("'%v' is already taken! Try another choice\n", intInput)
 	return false
-
 }
