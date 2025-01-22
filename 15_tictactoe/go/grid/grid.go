11dc01f24dd821bf3c4473c6a00ebd2f8dbd7be7
M <114847810+riraum@users.noreply.github.com>
2025-01-22 14:54:14 +0700

    WIP simplify IsWin function

--- 15_tictactoe/go/grid/grid.go
+++ 15_tictactoe/go/grid/grid.go
@@ -32,62 +32,63 @@ func (g Grid) String() string {
 // Create variable for empty cell, filled with X and with O
 // Return row of cells based on slice length, target index of slice and assign one of the 3 cell types
 // Return column of cells based and do the same as with the row
-func (g Grid) IsWinSimple() (bool, string) {
+func (g Grid) IsWin() (bool, string) {
 	// check horizontal
-	if g.Cells[0] == g.Cells[1] && g.Cells[0] == g.Cells[2] ||
-		g.Cells[3] == g.Cells[4] && g.Cells[3] == g.Cells[5] ||
-		g.Cells[6] == g.Cells[7] && g.Cells[6] == g.Cells[8] ||
+	if (g.Cells[0] == g.Cells[1] && g.Cells[0] == g.Cells[2]) ||
+		(g.Cells[3] == g.Cells[4] && g.Cells[3] == g.Cells[5]) ||
+		(g.Cells[6] == g.Cells[7] && g.Cells[6] == g.Cells[8]) ||
 		//  check vertical
-		g.Cells[0] == g.Cells[3] && g.Cells[0] == g.Cells[6] ||
-		g.Cells[1] == g.Cells[4] && g.Cells[1] == g.Cells[7] ||
-		g.Cells[2] == g.Cells[5] && g.Cells[2] == g.Cells[8] ||
+		(g.Cells[0] == g.Cells[3] && g.Cells[0] == g.Cells[6]) ||
+		(g.Cells[1] == g.Cells[4] && g.Cells[1] == g.Cells[7]) ||
+		(g.Cells[2] == g.Cells[5] && g.Cells[2] == g.Cells[8]) ||
 		// check diagonally
-		g.Cells[0] == g.Cells[4] && g.Cells[0] == g.Cells[8] ||
-		g.Cells[2] == g.Cells[4] && g.Cells[2] == g.Cells[6] {
+		(g.Cells[0] == g.Cells[4] && g.Cells[0] == g.Cells[8]) ||
+		(g.Cells[2] == g.Cells[4] && g.Cells[2] == g.Cells[6]) {
 		return true, "Winner!"
 	}
 	return false, "No winner"
 }
-func (g Grid) IsWin() (bool, string) {
-	// 1st row top to bottom horizontally
-	if g.Cells[0] == "X" && g.Cells[1] == "X" && g.Cells[2] == "X" ||
-		// 2nd row top to bottom horizontally
-		g.Cells[3] == "X" && g.Cells[4] == "X" && g.Cells[5] == "X" ||
-		// 3rd row top to bottom horizontally
-		g.Cells[6] == "X" && g.Cells[7] == "X" && g.Cells[8] == "X" ||
-		// 1st row top to bottom diagonally left to right
-		g.Cells[0] == "X" && g.Cells[4] == "X" && g.Cells[8] == "X" ||
-		// 2nd row top to bottom diagonally right to left
-		g.Cells[2] == "X" && g.Cells[4] == "X" && g.Cells[6] == "X" ||
-		// 1st row top to bottom vertically
-		g.Cells[0] == "X" && g.Cells[3] == "X" && g.Cells[6] == "X" ||
-		// 2nd row top to bottom vertically
-		g.Cells[1] == "X" && g.Cells[4] == "X" && g.Cells[7] == "X" ||
-		// 3rd row top to bottom vertically
-		g.Cells[2] == "X" && g.Cells[5] == "X" && g.Cells[8] == "X" {
-		return true, "Human won"
-	}
 
-	// 1st row top to bottom horizontally
-	if g.Cells[0] == "O" && g.Cells[1] == "O" && g.Cells[2] == "O" ||
-		// 2nd row top to bottom horizontally
-		g.Cells[3] == "O" && g.Cells[4] == "O" && g.Cells[5] == "O" ||
-		// 3rd row top to bottom horizontally
-		g.Cells[6] == "O" && g.Cells[7] == "O" && g.Cells[8] == "O" ||
-		// 1st row top to bottom diagonally left to right
-		g.Cells[0] == "O" && g.Cells[4] == "O" && g.Cells[8] == "O" ||
-		// 2nd row top to bottom diagonally right to left
-		g.Cells[2] == "O" && g.Cells[4] == "O" && g.Cells[6] == "O" ||
-		// 1st row top to bottom vertically
-		g.Cells[0] == "O" && g.Cells[3] == "O" && g.Cells[6] == "O" ||
-		// 2nd row top to bottom vertically
-		g.Cells[1] == "O" && g.Cells[4] == "O" && g.Cells[7] == "O" ||
-		// 3rd row top to bottom vertically
-		g.Cells[2] == "O" && g.Cells[5] == "O" && g.Cells[8] == "O" {
-		return true, "Computer won"
-	}
-	return false, "No winner"
-}
+// func (g Grid) IsWin() (bool, string) {
+// 	// 1st row top to bottom horizontally
+// 	if g.Cells[0] == "X" && g.Cells[1] == "X" && g.Cells[2] == "X" ||
+// 		// 2nd row top to bottom horizontally
+// 		g.Cells[3] == "X" && g.Cells[4] == "X" && g.Cells[5] == "X" ||
+// 		// 3rd row top to bottom horizontally
+// 		g.Cells[6] == "X" && g.Cells[7] == "X" && g.Cells[8] == "X" ||
+// 		// 1st row top to bottom diagonally left to right
+// 		g.Cells[0] == "X" && g.Cells[4] == "X" && g.Cells[8] == "X" ||
+// 		// 2nd row top to bottom diagonally right to left
+// 		g.Cells[2] == "X" && g.Cells[4] == "X" && g.Cells[6] == "X" ||
+// 		// 1st row top to bottom vertically
+// 		g.Cells[0] == "X" && g.Cells[3] == "X" && g.Cells[6] == "X" ||
+// 		// 2nd row top to bottom vertically
+// 		g.Cells[1] == "X" && g.Cells[4] == "X" && g.Cells[7] == "X" ||
+// 		// 3rd row top to bottom vertically
+// 		g.Cells[2] == "X" && g.Cells[5] == "X" && g.Cells[8] == "X" {
+// 		return true, "Human won"
+// 	}
+
+// 	// 1st row top to bottom horizontally
+// 	if g.Cells[0] == "O" && g.Cells[1] == "O" && g.Cells[2] == "O" ||
+// 		// 2nd row top to bottom horizontally
+// 		g.Cells[3] == "O" && g.Cells[4] == "O" && g.Cells[5] == "O" ||
+// 		// 3rd row top to bottom horizontally
+// 		g.Cells[6] == "O" && g.Cells[7] == "O" && g.Cells[8] == "O" ||
+// 		// 1st row top to bottom diagonally left to right
+// 		g.Cells[0] == "O" && g.Cells[4] == "O" && g.Cells[8] == "O" ||
+// 		// 2nd row top to bottom diagonally right to left
+// 		g.Cells[2] == "O" && g.Cells[4] == "O" && g.Cells[6] == "O" ||
+// 		// 1st row top to bottom vertically
+// 		g.Cells[0] == "O" && g.Cells[3] == "O" && g.Cells[6] == "O" ||
+// 		// 2nd row top to bottom vertically
+// 		g.Cells[1] == "O" && g.Cells[4] == "O" && g.Cells[7] == "O" ||
+// 		// 3rd row top to bottom vertically
+// 		g.Cells[2] == "O" && g.Cells[5] == "O" && g.Cells[8] == "O" {
+// 		return true, "Computer won"
+// 	}
+// 	return false, "No winner"
+// }
 
 func (g Grid) FreeCells() []int {
 	var resultIndexArray []int
