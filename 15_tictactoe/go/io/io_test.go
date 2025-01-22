dc7df90a61fdcbf6cad3663a621a9c4643ce2812
M <114847810+riraum@users.noreply.github.com>
2025-01-22 22:51:17 +0700

    Adjust tests to new logic

--- 15_tictactoe/go/io/io_test.go
+++ 15_tictactoe/go/io/io_test.go
@@ -6,17 +6,18 @@ import (
 
 func TestValid(t *testing.T) {
 	tests := []struct {
-		input string
-		want  bool
+		input     string
+		testSlice []int
+		want      bool
 	}{
-		{"3", true},
-		{"99", false},
-		{"", false},
-		{"abc", false},
+		{"3", []int{3}, true},
+		{"99", []int{}, false},
+		{"", []int{}, false},
+		{"abc", []int{}, false},
 	}
 
 	for _, test := range tests {
-		got := valid(test.input)
+		got := valid(test.input, []int{3})
 		if got != test.want {
 			t.Errorf("valid = %v, want %v", test.input, test.want)
 		}
