957624c2813a56eabfc62007a64527fd41779510
M <114847810+riraum@users.noreply.github.com>
2025-01-23 16:34:00 +0700

    IsWin, checkLine completed

--- 15_tictactoe/go/grid/grid.go
+++ 15_tictactoe/go/grid/grid.go
@@ -48,75 +48,12 @@ func (g Grid) checkLine(x, y, z int) (bool, string) {
 // Return row of cells based on slice length, target index of slice and assign one of the 3 cell types
 // Return column of cells based and do the same as with the row
 func (g Grid) IsWin() (bool, string) {
-	computerWin := "Computer wins!"
-	humanWin := "Human wins!"
 	// check horizontal
-	if g.Cells[0] == g.Cells[1] && g.Cells[0] == g.Cells[2] {
-		if g.Cells[0] == "X" {
-			return true, humanWin
-		}
-		if g.Cells[0] == "O" {
-			return true, computerWin
-		}
-	}
-	if g.Cells[3] == g.Cells[4] && g.Cells[3] == g.Cells[5] {
-		if g.Cells[3] == "X" {
-			return true, humanWin
-		}
-		if g.Cells[3] == "O" {
-			return true, computerWin
-		}
-	}
-	if g.Cells[6] == g.Cells[7] && g.Cells[6] == g.Cells[8] {
-		if g.Cells[6] == "X" {
-			return true, humanWin
-		}
-		if g.Cells[6] == "O" {
-			return true, computerWin
-		}
-	}
+	g.checkLine(0, 1, 2)
 	//  check vertical
-	if g.Cells[0] == g.Cells[3] && g.Cells[0] == g.Cells[6] {
-		if g.Cells[0] == "X" {
-			return true, humanWin
-		}
-		if g.Cells[0] == "O" {
-			return true, computerWin
-		}
-	}
-	if g.Cells[1] == g.Cells[4] && g.Cells[1] == g.Cells[7] {
-		if g.Cells[1] == "X" {
-			return true, humanWin
-		}
-		if g.Cells[1] == "O" {
-			return true, computerWin
-		}
-	}
-	if g.Cells[2] == g.Cells[5] && g.Cells[2] == g.Cells[8] {
-		if g.Cells[2] == "X" {
-			return true, humanWin
-		}
-		if g.Cells[2] == "O" {
-			return true, computerWin
-		}
-	}
+	g.checkLine(0, 3, 6)
 	// check diagonally
-	if g.Cells[0] == g.Cells[4] && g.Cells[0] == g.Cells[8] {
-		if g.Cells[0] == "X" {
-			return true, humanWin
-		}
-		if g.Cells[0] == "O" {
-			return true, computerWin
-		}
-	}
-	if g.Cells[2] == g.Cells[4] && g.Cells[2] == g.Cells[6] {
-		if g.Cells[2] == "X" {
-			return true, humanWin
-		}
-		if g.Cells[2] == "O" {
-			return true, computerWin
-		}
-	}
+	g.checkLine(0, 4, 8)
 	return false, "No winner"
 }
 
