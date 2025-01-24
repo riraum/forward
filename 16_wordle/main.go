df14c5fd9a1b7aef58605215531f165204204384
M <114847810+riraum@users.noreply.github.com>
2025-01-24 20:43:41 +0700

    Add user input, hard coded validity check

--- 16_wordle/main.go
+++ 16_wordle/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	var input string
+	fmt.Printf("Enter 5 digit word %s \n>", input)
+	fmt.Scan(&input)
+
+	if input == "Test" {
+		fmt.Print("Correct!\n")
+	} else {
+		fmt.Print("Not correct!\n")
+	}
 }
