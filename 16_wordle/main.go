9b3fdae1370a9c31baeb89e66ac65578787a5d1f
M <114847810+riraum@users.noreply.github.com>
2025-01-27 18:54:38 +0700

    WIP read file

--- 16_wordle/main.go
+++ 16_wordle/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
 	"slices"
+	"strings"
 )
 
 func valid(input string, wordSlice []string) bool {
@@ -13,29 +17,53 @@ func valid(input string, wordSlice []string) bool {
 }
 
 func main() {
-	var input string
-	var wl []string
-	wl = word_list.wordSlice
+	// var input string
+	// var wl []string
+	// wl = word_list.wordSlice
 
-	for i := 0; ; i++ {
-		fmt.Printf("Enter 5 digit word\n>")
-		fmt.Scan(&input)
+	// Read word list
+	data, err := os.ReadFile("word_list/word_list")
+	if err != nil {
+		log.Fatal(err)
+	}
+	os.Stdout.Write(data)
+	// debug
+	fmt.Print(data)
 
-		// for value, index := range wordSlice {
-		// 	if input == value {
-		if valid(input, word_list.wordSlice) {
-			fmt.Print("Correct word!\n")
-			break
-		} else {
-			fmt.Print("Not correct word, try again!\n")
-		}
+	// debug
+	scanner := bufio.NewScanner(strings.NewReader(string(data)))
+	scanner.Split(bufio.ScanWords)
+	count := 0
+	for scanner.Scan() {
+		count++
 	}
 
-	// func (slice []string) checkValid(input) bool {
-	// 	for value := range slice {
-	// 		if input ==  value {
-	// 			return true
-	// 		}
-	// 	}
-	// }
+	if err := scanner.Err(); err != nil {
+		fmt.Println(os.Stderr, "reading input:", err)
+	}
+	fmt.Printf("%d\n", count)
 }
+
+// for i := 0; ; i++ {
+// 	fmt.Printf("Enter 5 digit word\n>")
+// 	fmt.Scan(&input)
+
+// for value, index := range wordSlice {
+// 	if input == value {
+// 		// if valid(input, word_list.wordSlice) {
+// 		fmt.Print("Correct word!\n")
+// 		break
+// 	} else {
+// 		fmt.Print("Not correct word, try again!\n")
+// 	}
+// }
+
+// func (slice []string) checkValid(input) bool {
+// 	for value := range slice {
+// 		if input ==  value {
+// 			return true
+// 		}
+// 	}
+// }
+// }
+// }
