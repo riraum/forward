bf35cbd0d9533b5ea98fccb205c922a0c7035b83
M <114847810+riraum@users.noreply.github.com>
2025-01-27 19:14:18 +0700

    Comment out code

--- 16_wordle/main.go
+++ 16_wordle/main.go
@@ -1,20 +1,17 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"log"
 	"os"
-	"slices"
-	"strings"
 )
 
-func valid(input string, wordSlice []string) bool {
-	if slices.Contains(wordSlice, input) {
-		return true
-	}
-	return false
-}
+// func valid(input string, wordSlice []string) bool {
+// 	if slices.Contains(wordSlice, input) {
+// 		return true
+// 	}
+// 	return false
+// }
 
 func main() {
 	// var input string
@@ -29,20 +26,21 @@ func main() {
 	os.Stdout.Write(data)
 	// debug
 	fmt.Print(data)
-
-	// debug
-	scanner := bufio.NewScanner(strings.NewReader(string(data)))
-	scanner.Split(bufio.ScanWords)
-	count := 0
-	for scanner.Scan() {
-		count++
-	}
-
-	if err := scanner.Err(); err != nil {
-		fmt.Println(os.Stderr, "reading input:", err)
-	}
-	fmt.Printf("%d\n", count)
 }
+
+// debug
+// 	scanner := bufio.NewScanner(strings.NewReader(string(data)))
+// 	scanner.Split(bufio.ScanWords)
+// 	count := 0
+// 	for scanner.Scan() {
+// 		count++
+// 	}
+
+// 	if err := scanner.Err(); err != nil {
+// 		fmt.Println(os.Stderr, "reading input:", err)
+// 	}
+// 	fmt.Printf("%d\n", count)
+// }
 
 // for i := 0; ; i++ {
 // 	fmt.Printf("Enter 5 digit word\n>")
