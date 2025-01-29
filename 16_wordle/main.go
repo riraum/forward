6cd41faafcb847fb91156fd2c76b9dda46cb0c60
M <114847810+riraum@users.noreply.github.com>
2025-01-29 21:46:07 +0700

    Add chosenWord check

--- 16_wordle/main.go
+++ 16_wordle/main.go
@@ -27,12 +27,17 @@ func main() {
 	// fmt.Printf("%q\n", formattedData)
 	// fmt.Printf("%q\n", bytes.Split([]byte(data), []byte("\n")))
 	// fmt.Println(bytes.Contains([]byte("brown"), formattedData))
+	chosenWord := "brown"
+	chosenWordArray := []byte(chosenWord)
+
 	for i := 0; ; i++ {
 		fmt.Printf("Enter 5 letter word\n>")
 		fmt.Scan(&input)
 		// check if word on list
 		if isValid(input, words) {
-			break
+			if isChosen(input, chosenWordArray) {
+				break
+			}
 		}
 	}
 }
@@ -52,5 +57,13 @@ func isValid(word []byte, validWords [][]byte) bool {
 	}
 	// debug
 	fmt.Print("Invalid word, try again!\n")
+	return false
+}
+
+func isChosen(word, chosen []byte) bool {
+	if bytes.Equal(word, chosen) {
+		fmt.Print("Chosen word, yay!\n")
+		return true
+	}
 	return false
 }
