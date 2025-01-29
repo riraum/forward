9884ed7ddcd8232228567d95ec1df4ee9d10c538
M <114847810+riraum@users.noreply.github.com>
2025-01-29 19:02:08 +0700

    WIP input loop

--- 16_wordle/main.go
+++ 16_wordle/main.go
@@ -7,29 +7,92 @@ import (
 	"os"
 )
 
-// func valid(input string, wordSlice []string) bool {
-// 	if slices.Contains(wordSlice, input) {
+// func valid(input []byte) bool {
+// 	if bytes.Contains(input []byte) {
 // 		return true
 // 	}
 // 	return false
 // }
 
+// func (slice []string) checkValid(input) bool {
+// 	for value := range slice {
+// 		if input ==  value {
+// 			return true
+// 		}
+// 	}
+// }
+
 func main() {
-	// var input string
+	var input []byte
 	// var wl []string
 	// wl = word_list.wordSlice
-
 	// Read word list
 	data, err := os.ReadFile("word_list/word_list")
 	if err != nil {
 		log.Fatal(err)
 	}
-	os.Stdout.Write(data)
+	// os.Stdout.Write(data)
 	// debug
 	// fmt.Print(data)
 	// separate bytes
-	fmt.Printf("%q\n", bytes.Split([]byte(data), []byte("\n")))
+	formattedData := bytes.Split([]byte(data), []byte("\n"))
+	// debug
+	// fmt.Printf("%q\n", formattedData)
+	// fmt.Printf("%q\n", bytes.Split([]byte(data), []byte("\n")))
+	// fmt.Println(bytes.Contains([]byte("brown"), formattedData))
+	for i := 0; ; i++ {
+		fmt.Printf("Enter 5 digit word\n>")
+		fmt.Scan(&input)
+
+		for _, value := range formattedData {
+			// bytes.Equal(value, input)
+			// if valid(input, word_list.wordSlice) {
+			if bytes.Equal(value, input) {
+				fmt.Print("Correct word!\n")
+				// if valid(input, word_list.wordSlice) {
+				break
+			}
+			// debug
+			// fmt.Print("Not correct word!\n")
+			// range loop
+			// break
+		}
+		// input loop
+		// break
+	}
+	// end main
 }
+
+// func equal(input [][]byte, value []byte) bool {
+// 	for _, value := range formattedData {
+// 		bytes.Equal(value, input)
+// 		// if valid(input, word_list.wordSlice) {
+// 		if bytes.Equal(value, input) {
+// 			fmt.Print("Correct word!\n")
+// 			// if valid(input, word_list.wordSlice) {
+// 			return true
+// 		} else {
+// 			fmt.Print("Not correct word, try again!\n")
+// 			return false
+// 		}
+// 	}
+// }
+
+//
+// 	for _, value := range formattedData {
+// 		if bytes.Equal(value, input) {
+// 			// if valid(input, word_list.wordSlice) {
+// 			break
+// 		} else {
+// 			fmt.Print("Not correct word, try again!\n")
+// 			// break
+// 		}
+// 		// range loop
+// 		fmt.Print("Correct word!\n")
+// 		break
+// 	}
+// 	// input loop
+// }
 
 // debug
 // 	scanner := bufio.NewScanner(strings.NewReader(string(data)))
