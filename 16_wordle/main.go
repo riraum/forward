4b2056402d06b6afadf2794575f3bfc0d2a22280
M <114847810+riraum@users.noreply.github.com>
2025-01-30 00:47:50 +0700

    Simplify word loop, remove code

--- 16_wordle/main.go
+++ 16_wordle/main.go
@@ -7,13 +7,6 @@ import (
 	"os"
 )
 
-// func valid(input []byte) bool {
-// 	if bytes.Contains(input []byte) {
-// 		return true
-// 	}
-// 	return false
-// }
-
 func main() {
 	var input []byte
 
@@ -21,51 +14,35 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	// os.Stdout.Write(data)
+
 	words := bytes.Split([]byte(data), []byte("\n"))
-	// debug
-	// fmt.Printf("%q\n", formattedData)
-	// fmt.Printf("%q\n", bytes.Split([]byte(data), []byte("\n")))
-	// fmt.Println(bytes.Contains([]byte("brown"), formattedData))
 	chosenWord := "brown"
 	chosenWordArray := []byte(chosenWord)
 
 	for i := 0; ; i++ {
 		fmt.Printf("Enter 5 letter word\n>")
 		fmt.Scan(&input)
-		// check if word on list
-		if isValid(input, words) && isChosen(input, chosenWordArray) {
+		if isChosen(input, chosenWordArray) {
+			fmt.Print("Chosen word, yay!\n")
 			break
 		}
-		if isValid(input, words) && !isChosen(input, chosenWordArray) {
-			fmt.Print("But not chosen word, try again!\n")
+		if isValid(input, words) {
+			fmt.Print("Valid word, but not chosen word, try again!\n")
+			continue
 		}
-
+		fmt.Print("Invalid word, try again!\n")
 	}
 }
 
 func isValid(word []byte, validWords [][]byte) bool {
-	// if slices.Contains(validWords, word) {
-	// 	return true
-	// }
 	for _, value := range validWords {
-		// bytes.Equal(word, input)
-		// if valid(input, word_list.wordSlice) {
 		if bytes.Equal(word, value) {
-			fmt.Print("Valid word!\n")
-			// if valid(input, word_list.wordSlice) {
 			return true
 		}
 	}
-	// debug
-	fmt.Print("Invalid word, try again!\n")
 	return false
 }
 
 func isChosen(word, chosenWord []byte) bool {
-	if bytes.Equal(word, chosenWord) {
-		fmt.Print("Chosen word, yay!\n")
-		return true
-	}
-	return false
+	return bytes.Equal(word, chosenWord)
 }
