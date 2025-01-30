557e57606fd62cc2dfcfe070b18dfdd4f707dad1
M <114847810+riraum@users.noreply.github.com>
2025-01-30 16:27:56 +0700

    Improve test struct

--- 16_wordle/main_test.go
+++ 16_wordle/main_test.go
@@ -5,11 +5,6 @@ import (
 )
 
 func TestIsValid(t *testing.T) {
-	// data, err := os.ReadFile("word_list/word_list")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
 	word1 := []byte("brown")
 	word2 := []byte("aaaa")
 	word3 := []byte("possa")
@@ -17,15 +12,14 @@ func TestIsValid(t *testing.T) {
 	words := [][]byte{word1, word3}
 
 	tests := []struct {
-		word  []byte
-		words [][]byte
-		want  bool
+		word []byte
+		want bool
 	}{
-		{word1, words, true},
-		{word2, words, false},
+		{word1, true},
+		{word2, false},
 	}
 	for _, test := range tests {
-		got := isValid(test.word, test.words)
+		got := isValid(test.word, words)
 
 		if got != test.want {
 			t.Errorf("valid = %v, want %v", test.word, test.want)
@@ -40,12 +34,11 @@ func TestIsChosen(t *testing.T) {
 	chosenWord := word1
 
 	tests := []struct {
-		word       []byte
-		chosenWord []byte
-		want       bool
+		word []byte
+		want bool
 	}{
-		{word1, chosenWord, true},
-		{word2, chosenWord, false},
+		{word1, true},
+		{word2, false},
 	}
 	for _, test := range tests {
 		got := isChosen(test.word, chosenWord)
